83659df419a48d98bb84312474a8c3f218f07c20
Peter Garside-Quin <petegq@gmail.com>
2025-01-18 18:31:56 +0000

    Input source/backup dirs

--- file-backup.sh
+++ file-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-source_dir="/home/username/Documents"
-backup_dir="/media/username/Backup"
+read -p "Source directory:" source_dir
+read -p "Backup directory:" backup_dir
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 backup_file="backup-$timestamp.tar.gz"
-tar -czf $backup_dir/$backup_file $source_dir
+tar -czf $backup_file $source_dir
+mv $source_dir/$backup_file $backup_dir
